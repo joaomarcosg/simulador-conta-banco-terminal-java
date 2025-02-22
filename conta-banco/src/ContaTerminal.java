93af09aa500027739fbc84ce32bd33d0699e62a2
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-02-22 20:13:24 -0300

    desafio concluído: lógica para input de dados pelo usuário e saída para esses dados

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o seu nome: ");
+        String clientName = scanner.next();
+
+        System.out.println("Digite o número da agência: ");
+        String agencyNumber = scanner.next();
+
+        System.out.println("Digite o número da conta: ");
+        int countNumber = scanner.nextInt();
+
+        System.out.println("Digite seu saldo em conta: ");
+        double balance = scanner.nextDouble();
+
+        System.out.println("Olá " + clientName + ", obrigado por criar uma conta em nosso banco!");
+        System.out.println("Sua agência é: " + agencyNumber + " conta: " + countNumber);
+        System.out.println("E seu saldo R$: " + balance + " Já está disponível para saque.");
+
+
     }
 }
